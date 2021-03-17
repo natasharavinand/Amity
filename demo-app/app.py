18b12392072f2d201e530c88b8bbc629165bc685
natasharavinand <natasharavinand@gmail.com>
2021-03-16 22:22:56 -0700

    add demo_result file to demo-app

--- demo-app/app.py
+++ demo-app/app.py
@@ -1,16 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+import pickle
 
 app = Flask(__name__)
 
 app.debug = True
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
     return render_template("home.html")
 
-@app.route("/demo")
+@app.route("/demo", methods=["GET"])
 def demo():
     return render_template("demo.html")
+
+@app.route("/demo_result", methods=["POST"])
+def demo_result():
+    return render_template("demo_result.html")
     
 if __name__ == "__main__":
     app.run(debug=True)
